ffb157cb999828f1c5b1a57d52d244deaf7c947b
David Ortega <davidortegaiglesias@gmail.com>
2024-01-26 11:24:57 +0100

    add images and start the project

---
+++ AppPetronesDavidOrtegaIglesias/AppPetronesDavidOrtegaIglesias/SceneDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  SceneDelegate.swift
+//  AppPetronesDavidOrtegaIglesias
+//
+//  Created by David Ortega Iglesias on 22/1/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	var window: UIWindow?
+
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		// Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+		// If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+		// This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+		guard let scene = (scene as? UIWindowScene) else { return }
+		//******
+		let window = UIWindow(windowScene: scene)
+		
+		//Instanciamos el 1er View COntroller
+		let splashVC = SplashViewController()
+		//Instanciamos Navigation
+		let navigationController = UINavigationController(rootViewController: splashVC)
+		
+		window.rootViewController = navigationController
+		window.makeKeyAndVisible()
+		self.window = window
+	}	
+}
+
